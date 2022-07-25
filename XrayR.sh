31ae59aa11c720264fc164457cba7eb1f119e495
fengguowudi <18614071206@163.com>
2022-07-25 15:50:05 +0800

    Update XrayR.sh

--- XrayR.sh
+++ XrayR.sh
@@ -82,7 +82,7 @@ before_show_menu() {
 }
 
 install() {
-    bash <(curl -Ls https://raw.githubusercontents.com/fengguowudi/XrayR-script/master/install.sh)
+    bash <(curl -Ls https://raw.githubusercontent.com/fengguowudi/XrayR-script/master/install.sh)
     if [[ $? == 0 ]]; then
         if [[ $# == 0 ]]; then
             start
@@ -98,7 +98,7 @@ update() {
     else
         version=$2
     fi
-    bash <(curl -Ls https://raw.githubusercontents.com/fengguowudi/XrayR-script/master/install.sh) $version
+    bash <(curl -Ls https://raw.githubusercontent.com/fengguowudi/XrayR-script/master/install.sh) $version
     if [[ $? == 0 ]]; then
         echo -e "${green}更新完成，已自动重启 XrayR，请使用 XrayR log 查看运行日志${plain}"
         exit
@@ -247,11 +247,11 @@ show_log() {
 }
 
 install_bbr() {
-    bash <(curl -L -s https://raw.githubusercontents.com/chiakge/Linux-NetSpeed/master/tcp.sh)
+    bash <(curl -L -s https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh)
 }
 
 update_shell() {
-    wget -O /usr/bin/XrayR -N --no-check-certificate https://raw.githubusercontents.com/fengguowudi/XrayR-script/master/XrayR.sh
+    wget -O /usr/bin/XrayR -N --no-check-certificate https://raw.githubusercontent.com/fengguowudi/XrayR-script/master/XrayR.sh
     if [[ $? != 0 ]]; then
         echo ""
         echo -e "${red}下载脚本失败，请检查本机能否连接 Github${plain}"
